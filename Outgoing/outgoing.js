2bb10b57b758f3b13458ea54bfb6181869f69fb5
Dennis Johnson <densarjohn@gmail.com>
2020-05-23 00:32:35 +0530

    Added new outgoing map section

--- Outgoing/outgoing.js
+++ Outgoing/outgoing.js
@@ -1,8 +1,14 @@
 let slideIndex = 1;
 let selectedContinent = "asia";
 
+let showCountry = false;
+let list = [];
+
 window.onload = function () {
   const continents = document.getElementsByClassName("land");
+  const countries = document.getElementsByClassName("country");
+
+  list = document.getElementsByClassName("LC-list");
 
   for (continent of continents) {
     //Change fill colour onClick
@@ -22,15 +28,40 @@ window.onload = function () {
         document.getElementById(event.target.id).style.fill = "inherit";
     });
   }
+
+  for (country of countries) {
+    country.addEventListener("touchstart", (event) => {
+      if (event.target.children[1].className == "LC-list")
+        changeCountry(event.target.children[1]);
+    });
+  }
   changeContinent(selectedContinent);
 };
 
-function changeContinent(newContinent) {
+window.onresize = () => {
+  showCountry = false;
+  for (l of list) {
+    l.style.display = "none";
+  }
+};
+
+changeContinent = (newContinent) => {
   //reset fill & display of previous continent
   document.getElementById(selectedContinent).style.fill = "inherit";
   document.getElementById(`${selectedContinent}-card`).style.display = "none";
 
   selectedContinent = newContinent;
   document.getElementById(selectedContinent).style.fill = "white";
-  document.getElementById(`${selectedContinent}-card`).style.display = "block";
-}
+  document.getElementById(`${selectedContinent}-card`).style.display = "grid";
+};
+
+changeCountry = (newCountry) => {
+  if (showCountry) {
+    for (l of list) {
+      l.style.display = "none";
+    }
+  } else {
+    newCountry.style.display = "flex";
+  }
+  showCountry = !showCountry;
+};
