81b0431fdaa20ac4fc1fb40b7a86a531635ce2e6
Dennis Johnson <densarjohn@gmail.com>
2020-05-26 18:57:31 +0530

    Added testimonials section

--- Outgoing/outgoing.js
+++ Outgoing/outgoing.js
@@ -62,14 +62,15 @@ window.onload = function () {
   }
 
   changeContinent(selectedContinent);
+  showSlides(slideIndex);
 };
 
-window.onresize = () => {
-  // for (l of list) {
-  //   l.style.display = "none";
-  // }
-  showCountry = false;
-};
+// window.onresize = () => {
+//   // for (l of list) {
+//   //   l.style.display = "none";
+//   // }
+//   showCountry = false;
+// };
 
 changeContinent = (newContinent) => {
   //reset fill & display of previous continent
@@ -91,3 +92,45 @@ changeCountry = (newCountry) => {
   }
   showCountry = !showCountry;
 };
+
+let slideIndex = 1;
+
+window.onresize = () => {
+  showCountry = false;
+
+  const testimonial = document.getElementsByClassName("testimonial-wrapper");
+  for (t of testimonial) {
+    t.style.display = "none";
+  }
+  showSlides(slideIndex);
+};
+
+// window.onload = function () {
+//   showSlides(slideIndex);
+// };
+
+// Carousel --------------->
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName("testimonial-wrapper");
+  let dots = document.getElementsByClassName("carousel-dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
