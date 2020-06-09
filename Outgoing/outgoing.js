f7e88c49748ed449fb95831354631ad8cbb4332e
Dennis Johnson <densarjohn@gmail.com>
2020-06-09 23:41:55 +0530

    Added spinner to home page, links to About navBar

--- Outgoing/outgoing.js
+++ Outgoing/outgoing.js
@@ -6,9 +6,6 @@ let slideIndex = 1;
 let slideIncrement;
 
 window.onload = function () {
-  console.log("Loaded");
-  document.getElementById("loading-icon").style.display = "none";
-
   const continents = document.getElementsByClassName("land");
   const countries = document.getElementsByClassName("country");
 
